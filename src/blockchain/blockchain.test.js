1ddb72512b6dcdf88f582aea211831c07fb83e33
Carlos Azaustre <cazaustre@gmail.com>
2019-10-24 10:54:35 +0200

    test blockchain

---
+++ src/blockchain/blockchain.test.js
@@ -0,0 +1,26 @@
+import Blockchain from './blockchain';
+import Block from './block';
+
+describe('Blockchain', () => {
+  let blockchain;
+
+  beforeEach(() => {
+    blockchain = new Blockchain();
+  });
+
+  it('every blockchain has a genesis block', () => {
+    const [genesisBlock] = blockchain.blocks;
+
+    expect(genesisBlock).toEqual(Block.genesis);
+    expect(blockchain.blocks.length).toEqual(1);
+  });
+
+  it('use addBlock()', () => {
+    const data = 'dummy-data';
+    blockchain.addBlock(data);
+
+    const [, lastBlock] = blockchain.blocks;
+    expect(lastBlock.data).toEqual(data);
+    expect(blockchain.blocks.length).toEqual(2);
+  });
+});
