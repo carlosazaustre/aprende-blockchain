53ad2cf69d83adc3d7ed024db644469f36d11937
Carlos Azaustre <cazaustre@gmail.com>
2019-10-24 21:25:12 +0200

    /block endpoint

--- src/service/index.js
+++ src/service/index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
-import { log } from 'util';
 import Blockchain from '../blockchain';
 
 const { HTTP_PORT = 3000 } = process.env;
@@ -9,7 +8,13 @@ const { HTTP_PORT = 3000 } = process.env;
 const app = express();
 const blockchain = new Blockchain();
 
+blockchain.addBlock('express-block');
+
 app.use(bodyParser.json());
+
+app.get('/blocks', (req, res) => {
+  res.json(blockchain.blocks);
+});
 
 app.listen(HTTP_PORT, () => {
   console.log(`Service HTTP:${HTTP_PORT} listening...`);
