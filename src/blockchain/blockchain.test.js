ea652801af257fe5b1c252cb9ae102112203d494
Carlos Azaustre <cazaustre@gmail.com>
2019-10-24 11:51:05 +0200

    test: Blockchain replace()

--- src/blockchain/blockchain.test.js
+++ src/blockchain/blockchain.test.js
@@ -3,9 +3,11 @@ import Block from './block';
 
 describe('Blockchain', () => {
   let blockchain;
+  let blockchainB;
 
   beforeEach(() => {
     blockchain = new Blockchain();
+    blockchainB = new Blockchain();
   });
 
   it('every blockchain has a genesis block', () => {
@@ -22,5 +24,29 @@ describe('Blockchain', () => {
     const [, lastBlock] = blockchain.blocks;
     expect(lastBlock.data).toEqual(data);
     expect(blockchain.blocks.length).toEqual(2);
+  });
+
+  it('replaces the chain with a valid chain', () => {
+    blockchainB.addBlock('bl0ck-1');
+    blockchain.replace(blockchainB.blocks);
+
+    expect(blockchain.blocks).toEqual(blockchainB.blocks);
+  });
+
+  it('does not replace the chain with one with less blocks', () => {
+    blockchain.addBlock('bl0ck-1');
+
+    expect(() => {
+      blockchain.replace(blockchainB.blocks);
+    }).toThrowError('Received chain is not longer than current chain.');
+  });
+
+  it('not replace a chain with not valid one', () => {
+    blockchainB.addBlock('bl0ck-1');
+    blockchainB.blocks[1].data = 'b4d-d4t4';
+
+    expect(() => {
+      blockchain.replace(blockchainB.blocks);
+    }).toThrowError('Received chain is invalid.');
   });
 });
