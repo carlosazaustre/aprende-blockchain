4a94cb236d8d9e7ede2f4f3f47280186be342c5f
Carlos Azaustre <cazaustre@gmail.com>
2019-10-24 21:36:03 +0200

    /mine endpoint

--- src/service/index.js
+++ src/service/index.js
@@ -16,6 +16,16 @@ app.get('/blocks', (req, res) => {
   res.json(blockchain.blocks);
 });
 
+app.post('/mine', (req, res) => {
+  const { body: { data } } = req;
+  const block = blockchain.addBlock(data);
+
+  res.json({
+    blocks: blockchain.blocks.length,
+    block,
+  });
+});
+
 app.listen(HTTP_PORT, () => {
   console.log(`Service HTTP:${HTTP_PORT} listening...`);
 });
