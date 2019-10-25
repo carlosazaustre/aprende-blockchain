a7f8d4e4dcd0c8719b16bbfde8b2d85d97fe1009
Carlos Azaustre <cazaustre@gmail.com>
2019-10-25 19:58:24 +0200

    PoW and nonce

--- src/blockchain/block.js
+++ src/blockchain/block.js
@@ -1,33 +1,44 @@
 import { SHA256 } from 'crypto-js';
 
+const DIFFICULTY = 3;
+
 class Block {
-  constructor(timestamp, previousHash, hash, data) {
+  constructor(timestamp, previousHash, hash, data, nonce) {
     this.timestamp = timestamp;
     this.previousHash = previousHash;
     this.hash = hash;
     this.data = data;
+    this.nonce = nonce;
   }
 
   static get genesis() {
-    const timestamp = (new Date(2000, 0, 1)).getTime();
+    const timestamp = new Date(2000, 0, 1).getTime();
     return new this(timestamp, undefined, 'g3n3s1s-ha$h', 'i like ramen.');
   }
 
   static mine(previousBlock, data) {
     const timestamp = Date.now();
     const { hash: previousHash } = previousBlock;
-    const hash = Block.hash(timestamp, previousHash, data);
+    let hash;
+    let nonce = 0;
+    let timestamp;
+
+    do {
+      timestamp = Date.now();
+      nonce += 1;
+      hash = Block.hash(timestamp, previousHash, data, nonce);
+    } while (hash.substring(0, DIFFICULTY) !== '0'.repeat(DIFFICULTY));
 
     return new this(timestamp, previousHash, hash, data);
   }
-
-  static hash(timestamp, previousHash, data) {
-    return SHA256(`${timestamp}${previousHash}${data}`).toString();
+ 
+  static hash(timestamp, previousHash, data, nonce) {
+    return SHA256(`${timestamp}${previousHash}${data}${nonce}`).toString();
   }
 
   toString() {
     const {
-      timestamp, previousHash, hash, data,
+      timestamp, previousHash, hash, data, nonce,
     } = this;
 
     return `Block -
@@ -35,6 +46,7 @@ class Block {
       previousHash    : ${previousHash}
       hash            : ${hash}
       data            : ${data}
+      nonce           : ${nonce}
     `;
   }
 }
