3cf560a405898842bc8e74259f91c49f8a198ec1
Carlos Azaustre <cazaustre@gmail.com>
2019-10-25 17:47:11 +0200

    P2P service setup

---
+++ src/service/p2p.js
@@ -0,0 +1,24 @@
+import WebSocket from 'ws';
+
+const { P2P_PORT = 5000 } = process.env;
+
+class P2PService {
+  constructor(blockchain) {
+    this.blockchain = blockchain;
+    this.sockets = [];
+  }
+
+  listen() {
+    const server = new WebSocket.Server({ port: P2P_PORT });
+    server.on('connection', (socket) => this.onConnection(socket));
+
+    console.log(`Service ws:${P2P_PORT} listening...`);
+  }
+
+  onConnection(socket) {
+    console.log('[ws:socket] connected.');
+    this.sockets.push(socket);
+  }
+}
+
+export default P2PService;
