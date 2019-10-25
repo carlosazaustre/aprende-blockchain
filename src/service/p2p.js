7742f99ae56c11e9db046947c4efef78d6331763
Carlos Azaustre <cazaustre@gmail.com>
2019-10-25 19:12:10 +0200

    node broadcasting

--- src/service/p2p.js
+++ src/service/p2p.js
@@ -2,6 +2,7 @@ import WebSocket from 'ws';
 
 const { P2P_PORT = 5000, PEERS } = process.env;
 const peers = PEERS ? PEERS.split(',') : [];
+const MESSAGE = { BLOCKS: 'blocks' };
 
 class P2PService {
   constructor(blockchain) {
@@ -22,8 +23,25 @@ class P2PService {
   }
 
   onConnection(socket) {
+    const {
+      blockchain: { blocks },
+    } = this;
+
     console.log('[ws:socket] connected.');
     this.sockets.push(socket);
+    socket.on('message', (message) => {
+      const { type, value } = JSON.parse(message);
+
+      console.log({ type, value });
+    });
+
+    socket.send(JSON.stringify({ type: MESSAGE.BLOCKS, value: blocks }));
+  }
+
+  broadcast(type, value) {
+    console.log(`[ws:broadcast] ${type}...`);
+    const message = JSON.stringify({ type, value });
+    this.sockets.forEach((socket) => socket.send(message));
   }
 }
 
