f8f61294c768691dcdbf4fa3bbcad9d287e9f742
Carlos Azaustre <cazaustre@gmail.com>
2019-10-29 12:23:53 +0100

    class Transaction

---
+++ src/wallet/transaction.js
@@ -0,0 +1,25 @@
+import uuidV1 from 'uuid/v1';
+
+class Transaction {
+  constructor() {
+    this.id = uuidV1();
+    this.input = null;
+    this.outputs = [];
+  }
+
+  static create(senderWallet, recipientAddress, amount) {
+    const { balance, publicKey } = senderWallet;
+
+    if (amount > balance) throw Error(`Amount: ${amount} exceeds balance.`);
+
+    const transaction = new Transaction();
+    transaction.outputs.push(...[
+      { amount: balance - amount, address: publicKey },
+      { amount, address: recipientAddress },
+    ]);
+
+    return transaction;
+  }
+}
+
+export default Transaction;
