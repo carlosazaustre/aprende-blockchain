bdfba80120712de713bc18269df7e0f0740a5647
Carlos Azaustre <cazaustre@gmail.com>
2019-10-30 23:11:10 +0100

    update transactions

--- src/wallet/transaction.js
+++ src/wallet/transaction.js
@@ -14,25 +14,43 @@ class Transaction {
     if (amount > balance) throw Error(`Amount: ${amount} exceeds balance.`);
 
     const transaction = new Transaction();
-    transaction.outputs.push(...[
-      { amount: balance - amount, address: publicKey },
-      { amount, address: recipientAddress },
-    ]);
+    transaction.outputs.push(
+      ...[{ amount: balance - amount, address: publicKey }, { amount, address: recipientAddress }],
+    );
 
-    transaction.input = {
-      timestamp: Date.now(),
-      amount: senderWallet.balance,
-      address: senderWallet.publicKey,
-      signature: senderWallet.sign(transaction.outputs),
-    };
+    transaction.input = Transaction.sign(transaction, senderWallet);
 
     return transaction;
   }
 
   static verify(transaction) {
-    const { input: { address, signature }, outputs } = transaction;
+    const {
+      input: { address, signature },
+      outputs,
+    } = transaction;
 
     return elliptic.verifySignature(address, signature, outputs);
+  }
+
+  static sign(transaction, senderWallet) {
+    return {
+      timestamp: Date.now(),
+      amount: senderWallet.balance,
+      address: senderWallet.publicKey,
+      signature: senderWallet.sign(transaction.outputs),
+    };
+  }
+
+  update(senderWallet, recipientAddress, amount) {
+    const senderOutput = this.outputs.find((output) => output.address === senderWallet.publicKey);
+
+    if (amount > senderOutput.amount) throw Error(`Amount: ${amount} exceeds balance.`);
+
+    senderOutput.amount -= amount;
+    this.outputs.push({ amount, address: recipientAddress });
+    this.input = Transaction.sign(this, senderWallet);
+
+    return this;
   }
 }
 
