1dd36785e882b950484a638a8fff9219af51e1d0
Carlos Azaustre <cazaustre@gmail.com>
2019-10-30 23:02:17 +0100

    test: Transaction.verify()

--- src/modules/elliptic.js
+++ src/modules/elliptic.js
@@ -1,10 +1,10 @@
 import Elliptic from 'elliptic';
+import hash from './hash';
 
 const ec = new Elliptic.ec('secp256k1');
 
 export default {
   createKeyPair: () => ec.genKeyPair(),
 
-  verifySignature: (publicKey, signature, data) => ec.keyFromPublic(publicKey, 'hex').verify(),
-}
-;
+  verifySignature: (publicKey, signature, data) => ec.keyFromPublic(publicKey, 'hex').verify(hash(data), signature),
+};
--- src/wallet/transaction.test.js
+++ src/wallet/transaction.test.js
@@ -49,4 +49,13 @@ describe('Transaction', () => {
     expect(typeof transaction.input.signature).toEqual('object');
     expect(transaction.input.signature).toEqual(wallet.sign(transaction.outputs));
   });
+
+  it('validates a valid transaction', () => {
+    expect(Transaction.verify(transaction)).toBe(true);
+  });
+
+  it('invalidates a corrupt transaction', () => {
+    transaction.outputs[0].amount = 500;
+    expect(Transaction.verify(transaction)).toBe(false);
+  });
 });
