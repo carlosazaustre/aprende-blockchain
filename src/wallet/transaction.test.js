91e0287947bc80292480a871f4d3731a674043b4
Carlos Azaustre <cazaustre@gmail.com>
2019-10-30 23:20:33 +0100

    test update transactions

--- src/wallet/transaction.test.js
+++ src/wallet/transaction.test.js
@@ -58,4 +58,25 @@ describe('Transaction', () => {
     transaction.outputs[0].amount = 500;
     expect(Transaction.verify(transaction)).toBe(false);
   });
+
+  describe('and updating a transaction', () => {
+    let nextAmount;
+    let nextRecipient;
+
+    beforeEach(() => {
+      nextAmount = 3;
+      nextRecipient = 'n3xt-4ddr3ss';
+      transaction = transaction.update(wallet, nextRecipient, nextAmount);
+    });
+
+    it('substract the next amount from the senders wallet', () => {
+      const output = transaction.outputs.find(({ address }) => address === wallet.publicKey);
+      expect(output.amount).toEqual(wallet.balance - amount - nextAmount);
+    });
+
+    it('outputs an amount for the next recipient', () => {
+      const output = transaction.outputs.find(({ address }) => address === nextRecipient);
+      expect(output.amount).toEqual(nextAmount);
+    });
+  });
 });
