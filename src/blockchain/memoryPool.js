ca073632fcee784ce0060bd54c044140cc2d8e01
Carlos Azaustre <cazaustre@gmail.com>
2019-11-03 23:51:55 +0100

    test: addOrUpdate()

--- src/blockchain/memoryPool.js
+++ src/blockchain/memoryPool.js
@@ -5,7 +5,7 @@ class MemoryPool {
 
   addOrUpdate(transaction) {
     const txIndex = this.transactions.findIndex(({ id }) => id === transaction.id);
-    if (txIndex > 0) this.transactions[txIndex] = transaction;
+    if (txIndex >= 0) this.transactions[txIndex] = transaction;
     else this.transactions.push(transaction);
   }
 }
