9fa152cb85ed8e612bce9fea00ac23804b80a259
Carlos Azaustre <cazaustre@gmail.com>
2019-11-04 00:03:26 +0100

    create transactions with Wallet

--- src/wallet/wallet.js
+++ src/wallet/wallet.js
@@ -1,16 +1,14 @@
+import Transaction from './transaction';
 import { elliptic, hash } from '../modules';
 
 const INITIAL_BALANCE = 100;
 
 class Wallet {
-  constructor() {
+  constructor(blockchain) {
     this.balance = INITIAL_BALANCE;
     this.keyPair = elliptic.createKeyPair();
     this.publicKey = this.keyPair.getPublic().encode('hex');
-  }
-
-  sign(data) {
-    return this.keyPair.sign(hash(data));
+    this.blockchain = blockchain;
   }
 
   toString() {
@@ -19,6 +17,25 @@ class Wallet {
       publicKey:    ${publicKey.toString()}
       balance:      ${balance}
     `;
+  }
+
+  sign(data) {
+    return this.keyPair.sign(hash(data));
+  }
+
+  createTransaction(recipientAddress, amount) {
+    const { balance, blockchain: { memoryPool } } = this;
+
+    if (amount > balance) throw Error(`Amount: ${amount} exceeds the current balance: ${balance}`);
+    let tx = memoryPool.find(this.publicKey);
+    if (tx) {
+      tx.update(this, recipientAddress, amount);
+    } else {
+      tx = Transaction.create(this, recipientAddress, amount);
+      memoryPool.addOrUpdate(tx);
+    }
+
+    return tx;
   }
 }
 
